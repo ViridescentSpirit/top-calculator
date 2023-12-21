abb127a17e07f1dcdf4191f877d8abcb50a9a25f
Lane Bye <viridescentspiritgaming@gmail.com>
2023-12-21 16:01:02 -0600

    Polish calculator functions
    Added logic to js file to handle equals, clear, and clear all buttons.
    Added rounding to math functions to limit to two decimal places. Will
    likely return to this project to add the decimal operations and limit
    the length of numeric inputs and outputs, but feel I have grasped all
    of the techniques being tested with this project, and feel confident
    to move forward into the next sections to learn additional methods and
    begin development of my end goal... a full website for my sports
    analytics.

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let a = 6
-let b = 3
-
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = ``;
@@ -59,20 +56,61 @@ function appendNumber(inputNumber) {
 
 const operButtons = document.querySelectorAll(`.operButton`);
 const history = document.querySelector(`.history`);
+let equalPressed = false;
 
 operButtons.forEach(button => {
     button.addEventListener(`click`, () => {
         if (firstNumber === 0) {
             operator = button.id;
             firstNumber = currentNumber;   
+        } else if(operator === `` && firstNumber != 0) {
+            operator = button.id;
+        } else if(equalPressed === true) {
+            operator = button.id;
+            firstNumber = currentNumber;
         } else {
             secondNumber = currentNumber;
-            firstNumber = operate(firstNumber, secondNumber, operator);
+            firstNumber = Math.round(operate(firstNumber, secondNumber, operator) * 100) / 100;
             operator = button.id;
         }
 
         history.textContent = `${firstNumber}  ${operator}`;
         currentNumDisplay.textContent = firstNumber;
         currentNumber = ``;
+        equalPressed = false;
     });
+});
+
+const equalsButton = document.querySelector(`.equalsButton`);
+
+equalsButton.addEventListener(`click`, () => {
+    if(firstNumber === 0) {
+        firstNumber = currentNumber;
+        history.textContent = `${firstNumber}  =`
+    } else {
+        secondNumber = currentNumber;
+        currentNumber = Math.round(operate(firstNumber, secondNumber, operator) * 100) / 100;
+        history.textContent = `${firstNumber}  ${operator}  ${secondNumber}  =`;
+    } 
+    currentNumDisplay.textContent = currentNumber;
+    firstNumber = currentNumber;
+    equalPressed = true;
+});
+
+const clearButton = document.querySelector(`#clearButton`);
+
+clearButton.addEventListener(`click`, () => {
+    currentNumDisplay.textContent = ``;
+    currentNumber = ``;
+});
+
+const clearEverythingButton = document.querySelector(`#clearEverythingButton`);
+
+clearEverythingButton.addEventListener(`click`, () => {
+    firstNumber = 0;
+    secondNumber = 0;
+    operator = ``;
+    currentNumber = ``;
+    currentNumDisplay.textContent = ``;
+    history.textContent = ``;
 });
