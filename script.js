e1d29d581a48e262aab3f59e4a6af117e935888f
Lane Bye <viridescentspiritgaming@gmail.com>
2023-12-21 10:26:50 -0600

    Create files for calculator project
    Created html, css, and js files for the calculator project. Added header
    to html and created style rules for h1 elements. Created initial four
    function calculator functions to be used when calculator elements are
    built. Tested to verify readout is correct.

---
+++ script.js
@@ -0,0 +1,23 @@
+let a = 6
+let b = 3
+
+function addition(a,b) {
+    return a + b;
+}
+
+function subtraction(a,b) {
+    return a - b;
+}
+
+function multiplication(a,b) {
+    return a * b;
+}
+
+function division(a,b) {
+    return a / b;
+}
+
+console.log(addition(a,b));
+console.log(subtraction(a,b));
+console.log(multiplication(a,b));
+console.log(division(a,b));
