4546eb88aeac71941c8fb7d1f3d9c6638fe6f697
Lane Bye <viridescentspiritgaming@gmail.com>
2023-12-21 11:58:00 -0600

    Create calculator front end
    Built html structure for calculator and it's buttons. Styled with CSS
    and used flexbox to arrange items into a user friendly layout. Created
    div for display but have not populated with any text content as js
    will be set up to drive calculator functions.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let a = 6
 let b = 3
 
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = ``;
+
 function addition(a,b) {
     return a + b;
 }
@@ -17,7 +21,24 @@ function division(a,b) {
     return a / b;
 }
 
-console.log(addition(a,b));
-console.log(subtraction(a,b));
-console.log(multiplication(a,b));
-console.log(division(a,b));
+function operate(firstNumber, secondNumber, operator) {
+    switch(operator) {
+            case `+`:
+                addition(firstNumber, secondNumber);
+                break;
+            case `-`:
+                subtraction(firstNumber, secondNumber);
+                break;
+            case `*`:
+                multiplication(firstNumber, secondNumber);
+                break;
+            case `/`:
+                division(firstNumber, secondNumber);
+                break;
+    }
+}
+
+// console.log(addition(a,b));
+// console.log(subtraction(a,b));
+// console.log(multiplication(a,b));
+// console.log(division(a,b));
